caf3e5a4a59a5230c25573fd151561d8a024f0e9
Sanyam Jain <sanyamjaino98@gmail.com>
2021-05-19 01:45:27 +0530

    Connected Sequelize

--- util/db.js
+++ util/db.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize') //gives us a constructor lets say
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-online-shop',
-    password: 'root'
-})
+//below sets us a connection pool. Dialect is saying we are using mysql db and host is localhost. Also first three params are db name, username and password
 
-module.exports = pool.promise()
+const sequelize = new Sequelize('node-online-shop', 'root', 'root', { dialect: 'mysql', host: 'localhost' })
+
+module.exports = sequelize
+
+//Below was used before implementing sequelize
+// const mysql = require('mysql2')
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-online-shop',
+//     password: 'root'
+// })
+
+// module.exports = pool.promise()
