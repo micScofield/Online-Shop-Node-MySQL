1e171cd5a819dfea02f7c533b413043a563c3a71
Sanyam Jain <sanyamjaino98@gmail.com>
2021-05-20 17:36:27 +0530

    Added session clearance on logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,3 +11,12 @@ exports.postLogin = (req, res, next) => {
     req.session.isLoggedIn = true
     res.redirect('/')
 };
+
+exports.postLogout = (req, res, next) => {
+    //clear out session
+    req.session.destroy((err) => {
+        if(err) console.error(err)
+        //session cookie on browser still exists but wont be valid because we have removed session from our db. It will be removed when browser is closed. No worries.
+        res.redirect('/')
+    })
+};
